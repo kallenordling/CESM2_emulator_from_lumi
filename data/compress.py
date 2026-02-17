5f603e8e7a6f4d9660bb666c4ce5b98674dc9365
nordling <nordlingkalle@gmail.com>
2026-02-17 12:53:23 +0200

    update normalization

--- data/compress.py
+++ data/compress.py
@@ -30,3 +30,16 @@ ds['CO2'].to_netcdf(
     engine="netcdf4",
     encoding=encoding
 )
+
+import numpy as np
+
+arr = ds['CO2'].values   # 3D array
+
+np.savetxt(
+    "array.txt",
+    arr.reshape(arr.shape[0], -1)  # flatten last dims
+)
+
+# save shape separately
+with open("array_shape.txt", "w") as f:
+    f.write(" ".join(map(str, arr.shape)))
