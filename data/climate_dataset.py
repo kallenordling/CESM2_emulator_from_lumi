ee2be807e2c18e9de699c6a80e388a335cc9e5ad
nordling <nordlingkalle@gmail.com>
2026-02-18 14:38:28 +0200

    update normalization

--- data/climate_dataset.py
+++ data/climate_dataset.py
@@ -51,9 +51,9 @@ def norm_zscore(da: xr.DataArray) -> xr.DataArray:
     """
     vals=da.values.flatten()
     if da.name == "SUL":
-        mask = vals > 1e-9
+        mask = vals > 1e-13
     if da.name == "CO2":
-        mask = vals > 1e-3
+        mask = vals > 1e-8
     vals=np.log1p(vals)
     mu    = float(vals[mask].mean())
     sigma = float(vals[mask].std())
