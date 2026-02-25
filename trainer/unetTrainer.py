39aa572307f77921e65b7914832b8a5aa9e3c4cb
nordling <nordlingkalle@gmail.com>
2026-02-25 09:21:48 +0200

    update normalization

--- trainer/unetTrainer.py
+++ trainer/unetTrainer.py
@@ -248,9 +248,9 @@ class UNetTrainer:
                             self.save(epoch)
 
                         # Metric calculation and logging
-                        avg_loss = self.accelerator.gather_for_metrics(loss).mean()
-                        avg_mse_loss = self.accelerator.gather_for_metrics(mse_loss).mean()
-                        avg_cond_loss = self.accelerator.gather_for_metrics(cond_loss).mean()
+                    avg_loss = self.accelerator.gather_for_metrics(loss).mean()
+                    avg_mse_loss = self.accelerator.gather_for_metrics(mse_loss).mean()
+                    avg_cond_loss = self.accelerator.gather_for_metrics(cond_loss).mean()
 
                     log_dict = {"Training/Loss": avg_loss.detach().item(),
                                     "MSE LOSS": avg_mse_loss.detach().item(),
