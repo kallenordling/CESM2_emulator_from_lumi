f1678d1e088e0771d41860b04610d679c87ae9f0
nordling <nordlingkalle@gmail.com>
2026-02-26 19:55:02 +0200

    update normalization

--- run2_aero.sh
+++ run2_aero.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=diffusion_aero
 #SBATCH --account=project_462001112
-#SBATCH --partition=standard-g
+#SBATCH --partition=dev-g
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=56
 #SBATCH --gpus-per-node=8
 #SBATCH --mem=128G
-#SBATCH --time=40:00:00
+#SBATCH --time=0:30:00
 #SBATCH --output=logs/%x_%j.out
 
 set -euo pipefail
